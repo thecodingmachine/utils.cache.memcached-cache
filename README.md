21c6f8470afa7cb1fd6f6ddce755c7682a12236c
David Négrier <d.negrier@thecodingmachine.com>
2015-01-07 19:00:05 +0100

    Migration to Mouf 2

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ Mouf Memcached cache service
 
 This package contains an implementation of Mouf's CacheInterface for Memcache, using the **memcached** pecl package.
 To learn more about the cache interface, please see the [cache system documentation](http://mouf-php.com/packages/mouf/utils.cache.cache-interface).
+
+This package comes with a default installer that will create a "memcacheCacheService" instance that points
+to a Memcache server on 127.0.0.1 listening on port 11211.
+
+To use it:
+
+```php
+Mouf::getMemcacheCacheService()->set('mykey', 'myvalue');
+
+$mykey = Mouf::getMemcacheCacheService()->get('mykey');
+```
--- src/Mouf/Utils/Cache/MemcachedCacheInstaller.php
+++ src/Mouf/Utils/Cache/MemcachedCacheInstaller.php
@@ -22,7 +22,17 @@ class MemcachedCacheInstaller implements PackageInstallerInterface {
 	 * @see \Mouf\Installer\PackageInstallerInterface::install()
 	 */
 	public static function install(MoufManager $moufManager) {
-		// TODO
+		// Let's create the instances.
+		$memcacheCacheService = InstallUtils::getOrCreateInstance('memcacheCacheService', 'Mouf\\Utils\\Cache\\MemcachedCache', $moufManager);
+		
+		// Let's bind instances together.
+		if (!$memcacheCacheService->getConstructorArgumentProperty('servers')->isValueSet()) {
+			$memcacheCacheService->getConstructorArgumentProperty('servers')->setValue(array(0 => '127.0.0.1:11211', ));
+		}
+		if (!$memcacheCacheService->getConstructorArgumentProperty('defaultTimeToLive')->isValueSet()) {
+			$memcacheCacheService->getConstructorArgumentProperty('defaultTimeToLive')->setValue('3600');
+		}
+
 		
 		// Let's rewrite the MoufComponents.php file to save the component
 		$moufManager->rewriteMouf();
